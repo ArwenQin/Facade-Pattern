1f5653c2f696eeb45918f164cf9a0e22aa0609fd
whl961210 <64228749+whl961210@users.noreply.github.com>
2023-04-15 21:50:05 -0700

    Update FacadeImpl.java

--- src/building/FacadeImpl.java
+++ src/building/FacadeImpl.java
@@ -1,46 +1,108 @@
 package building;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * This class implements Facade. It organizes the methods/operations in sub-systems and provides
- * simplified methods to Client.
+ * This class represents a facade implementation for controlling a building's appliances. It
+ * provides methods for turning on and off various appliances for a single floor, all floors, or the
+ * entire building.
  */
 public class FacadeImpl implements Facade {
-  private final Floor f1;
-  private final Floor f2;
-  private final Floor f3;
-  private final Floor f4;
+  private final Map<Integer, Floor> floors;
 
   /**
-   * Constructs a FacadeImpl object.
+   * Constructs a new FacadeImpl object. Initializes a map of floors with the associated Floor
+   * objects.
    */
   public FacadeImpl() {
-    f1 = new Floor1();
-    f2 = new Floor2();
-    f3 = new Floor3();
-    f4 = new Floor4();
+    floors = new HashMap<>();
+    floors.put(1, new Floor1());
+    floors.put(2, new Floor2());
+    floors.put(3, new Floor3());
+    floors.put(4, new Floor4());
+  }
+
+  @Override public String turnOnFloor(int floorNumber) {
+    checkFloorNumber(floorNumber);
+    return turnOnAllAppliancesForFloor(floorNumber);
+  }
+
+  @Override public String turnOffFloor(int floorNumber) {
+    checkFloorNumber(floorNumber);
+    return turnOffAllAppliancesForFloor(floorNumber);
+  }
+
+  private void checkFloorNumber(int floorNumber) {
+    if (floorNumber < 1 || floorNumber > 4) {
+      throw new IllegalArgumentException("Invalid floor number: " + floorNumber);
+    }
+  }
+
+  private Floor getFloor(int floorNumber) {
+    return floors.get(floorNumber);
   }
 
   @Override public String turnOnAllLights() {
-    return f1.turnOnLights() + f2.turnOnLights() + f3.turnOnLights() + f4.turnOnLights();
+    StringBuilder sb = new StringBuilder();
+    for (int i = 1; i <= 4; i++) {
+      sb.append(turnOnFloor(i));
+    }
+    return sb.toString();
   }
 
   @Override public String turnOnAllAirConditioners() {
-    return f1.turnOnAirConditioner() + f2.turnOnAirConditioner() + f3.turnOnAirConditioner()
-        + f4.turnOnAirConditioner();
+    StringBuilder sb = new StringBuilder();
+    for (int i = 1; i <= 4; i++) {
+      sb.append(getFloor(i).turnOnAirConditioner());
+    }
+    return sb.toString();
   }
 
   @Override public String turnOffAllPrinters() {
-    return f1.turnOffPrinter() + f2.turnOffPrinter() + f3.turnOffPrinter() + f4.turnOffPrinter();
+    StringBuilder sb = new StringBuilder();
+    for (int i = 1; i <= 4; i++) {
+      sb.append(getFloor(i).turnOffPrinter());
+    }
+    return sb.toString();
   }
 
   @Override public String turnOnAllBuilding() {
-    return f1.turnOnLights() + f1.turnOnAirConditioner() + f1.turnOnPrinter() + f2.turnOnLights()
-        + f2.turnOnAirConditioner() + f2.turnOnPrinter() + f3.turnOnLights()
-        + f3.turnOnAirConditioner() + f3.turnOnPrinter() + f4.turnOnLights()
-        + f4.turnOnAirConditioner() + f4.turnOnPrinter();
+    StringBuilder sb = new StringBuilder();
+    for (int i = 1; i <= 4; i++) {
+      sb.append(turnOnFloor(i));
+    }
+    return sb.toString();
   }
 
-  @Override public String turnOnFloor1() {
-    return f1.turnOnLights() + f1.turnOnAirConditioner() + f1.turnOnPrinter();
+  /**
+   * Helper method to turn on all appliances for a given floor.
+   *
+   * @param floorNumber the number of the floor to turn on the appliances for.
+   * @return a message indicating the success or failure of the operation.
+   */
+  private String turnOnAllAppliancesForFloor(int floorNumber) {
+    Floor floor = getFloor(floorNumber);
+    StringBuilder sb = new StringBuilder();
+    sb.append(floor.turnOnLights());
+    sb.append(floor.turnOnAirConditioner());
+    sb.append(floor.turnOnPrinter());
+    return sb.toString();
+  }
+
+  /**
+   * Helper method to turn off all appliances for a given floor.
+   *
+   * @param floorNumber the number of the floor to turn off the appliances for.
+   * @return a message indicating the success or failure of the operation.
+   */
+  private String turnOffAllAppliancesForFloor(int floorNumber) {
+    Floor floor = getFloor(floorNumber);
+    StringBuilder sb = new StringBuilder();
+    sb.append(floor.turnOffLights());
+    sb.append(floor.turnOffAirConditioner());
+    sb.append(floor.turnOffPrinter());
+    return sb.toString();
   }
 }
+
